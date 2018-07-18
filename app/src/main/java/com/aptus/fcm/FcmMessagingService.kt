60c84b005aec70a7284544bce5a2a9fb63836763
Shanmugasanthosh <shanmugasanthosh@outlook.com>
2018-07-18 11:11:25 +0530

    Remove unused comment

--- app/src/main/java/com/aptus/fcm/FcmMessagingService.kt
+++ app/src/main/java/com/aptus/fcm/FcmMessagingService.kt
@@ -13,13 +13,13 @@ class FcmMessagingService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String?) {
         super.onNewToken(token)
-        // This method receives Token for send message to devices.
+
         sendTokenToRegisterOnServer(token)
     }
 
 
     private fun sendTokenToRegisterOnServer(token: String?) {
-        // Handle your business logic
+        // Handle your logic
     }
 
     private fun onMessageReceive() {
