a4ddf784507e0bfa95c8470a7fb22cb4304ee666
Shanmugasanthosh <shanmugasanthosh@outlook.com>
2020-06-22 11:14:03 +0530

    Updated dependencies for firebase messaging.

--- app/src/main/java/com/aptus/fcm/FcmMessagingService.kt
+++ app/src/main/java/com/aptus/fcm/FcmMessagingService.kt
@@ -5,20 +5,20 @@ import com.google.firebase.messaging.RemoteMessage
 
 class FcmMessagingService : FirebaseMessagingService() {
 
-    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
         super.onMessageReceived(remoteMessage)
         // This method receives Notification and Data Payload
         onMessageReceive()
     }
 
-    override fun onNewToken(token: String?) {
+    override fun onNewToken(token: String) {
         super.onNewToken(token)
 
         sendTokenToRegisterOnServer(token)
     }
 
 
-    private fun sendTokenToRegisterOnServer(token: String?) {
+    private fun sendTokenToRegisterOnServer(token: String) {
         // Handle your logic
     }
 
